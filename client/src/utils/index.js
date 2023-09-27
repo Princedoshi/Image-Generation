c0b8579015d4c4a83a263573a42a832634e8ef87
PRINCE <pdoshi135@gmail.com>
2023-09-27 12:10:24 +0530

    /

---
+++ client/src/utils/index.js
@@ -0,0 +1,19 @@
+import {surpriseMePrompts} from '../constants';
+import Filesaver from 'file-saver'
+
+export function getRandomPrompt(prompt){
+
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex]
+
+    if(randomPrompt === prompt) return getRandomPrompy(prompt)
+
+    return randomPrompt;
+
+
+}
+
+
+export async function downloadImage(_id,photo){
+    Filesaver.saveAs(photo , `download-${_id}.jpg`)
+}
